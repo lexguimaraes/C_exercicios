f0090e9464d1f49515ee2f3e6b6a5f3159bd6b36
lexguimaraes <alexfsg@id.uff.br>
2024-06-08 03:03:09 -0300

    update

--- 1449b.c
+++ 1449b.c
@@ -39,6 +39,17 @@ char* lista_busca(Node* l, char* palavra){
     return palavra;
 }
 
+int lista_buscaind(Node*l, char*palavra){
+    int cont = 0;
+    for(Node *p = l;p!= NULL; p = p->next){
+        if (strcmp(p->data,palavra)==0){
+            return cont;
+        }
+        cont++;
+    }
+    return -1;
+}
+
 void lista_exclui (Node* l){
     Node* p = l; // ponteiro para o primeiro elemento
     while (p != NULL) { // enquanto não é o último elemento
@@ -57,33 +68,55 @@ int main(){
         Node* head = NULL;
         int n, m;
         scanf("%d %d\n", &n, &m);
-        char musica[m][81];
-        char palavra[64];
+        char musica[m][100];
+        char palavra[100]= {0};
         for (int j = 0; j < n * 2; j++) {
-            fgets(palavra,62,stdin);
-            palavra[strcspn(palavra,"\n")]=0;
+            //fgets(palavra,90,stdin);
+            scanf(" %[^\n]",palavra);
+            //palavra[strcspn(palavra,"\n")]=0;
             head = insere(head, palavra);
             if (head == NULL){
                 fprintf(stderr, "Memory allocation failed\n");
                 return 0;
             }
         }
+        memset(palavra,0,sizeof(palavra));
         for(int y = 0;y<m;y++){
-            fgets(palavra,62,stdin);
-            strcpy(musica[y],palavra);
+            //fgets(palavra,90,stdin);
+            scanf(" %[^\n]",palavra);
+            //palavra[strcspn(palavra,"\n")]=0;
+            //strcpy(musica[y],palavra);
             const char s[2] = " ";
             char *token;
             char trad[100];
-            token = strtok(musica[y], s);
+            if (!strlen(palavra))
+                printf("\n");
+            else {
+                token = strtok(palavra, s);
+                if ((lista_buscaind(head, token) % 2) != 0) {
 
-            while( token != NULL ) {
-                token[strcspn(token,"\n")]=0;
-                //printf("%s ",token);
-                strcpy(trad,lista_busca(head, token));
-                printf("%s ",trad);
-                token = strtok(NULL,s);
+                    strcpy(trad, lista_busca(head, token));
+                    printf("%s", trad);
+                } else printf("%s", token);
+                //token[strcspn(token, "\n")] = 0;
+                do {
+                    //token[strcspn(token, "\n")] = 0;
+                    //printf("%s ",token);
+                    //printf("%s %d\n",token, lista_buscaind(head,token));
+                    token = strtok(NULL, s);
+                    if(token)
+                        printf(" ");
+                    if (token){
+                        if ((lista_buscaind(head, token) % 2) != 0) {
+
+                            strcpy(trad, lista_busca(head, token));
+                            printf("%s", trad);
+                        } else printf("%s", token);
+                    }
+                }while(token!=NULL);
             }
             printf("\n");
+            memset(palavra,0,sizeof(palavra));
         }
         printf("\n");
         lista_exclui(head);
